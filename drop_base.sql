c5804abd9a55ebb623a1c1f5192909a95a23145e
BlisterBoy <mehdi.abelha@gmail.com>
2015-04-21 15:32:32 +0200

    aij daidjaz :

---
+++ drop_base.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS 
+	Message,
+	Contenu_Message,
+	Type_place,
+	Classe_Prix,
+	Evenement_Culturel,
+	Piece_Theatre,
+	Exposition,
+	Festival, 
+	date_evenement, 
+	Lieu, 
+	Ville, 
+	Membre, 
+	Administrateur,
+	avoir
+	
+	CASCADE ;
---
+++ requetes_ajouts.sql
@@ -0,0 +1,40 @@
+-- Membre
+
+
+-- Piece Theatre
+
+
+-- Exposition
+
+
+-- Festival
+
+
+-- Programmer date UNIQUE d'evenement
+
+
+-- Programmer date RECURRENT d'evenement
+
+
+-- Message
+
+
+-- Lieu
+
+
+-- Ville
+
+
+-- Type de Place
+
+
+-- Classe de Prix
+
+
+-- Avoir
+
+
+-- Animation
+
+
+-- Concert
