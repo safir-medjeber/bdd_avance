40b977ff4b81f0732961593d99942a239c7a26ec
BlisterBoy <mehdi.abelha@gmail.com>
2015-04-21 14:43:50 +0200

    Travail sur la bdd

--- init_base.sql
+++ init_base.sql
@@ -1,35 +1,46 @@
 -- Zone des Drop --
+\i drop_base.sql
 
-
-
--- Mehdi
-CREATE TABLE Message (
-	id_message 				SERIAL PRIMARY KEY,
-	id_membre				PRIMARY KEY REFERENCES Membre,
-	date_message			DATETIME NOT NULL,
-	id_contenu_message 		PRIMARY KEY REFERENCES Contenu_Message
+CREATE TABLE Membre (
+       id_membre 			SERIAL PRIMARY KEY,
+       nom_membre 			VARCHAR NOT NULL,
+       prenom_membre 		VARCHAR NOT NULL,
+       password_membre 		VARCHAR NOT NULL,
+       pseudo_membre 		VARCHAR NOT NULL,
+       mail_membre 			VARCHAR NOT NULL,
+       adresse_membre  		VARCHAR NOT NULL,
+       code_postal_membre	INTEGER NOT NULL,
+       estGarcon_membre		BOOLEAN NOT NULL
 );
+CREATE TABLE Administrateur (
+) INHERITS (Membre);
+
+CREATE TABLE Ville (
+       id_ville				SERIAL PRIMARY KEY,
+       code_postal_ville	INTEGER NOT NULL,
+       nom_ville 			VARCHAR NOT NULL
+);
+CREATE TABLE Lieu (
+       id_lieu				SERIAL PRIMARY KEY,
+       nom_lieu 			VARCHAR NOT NULL,
+       adress_lieu  		VARCHAR NOT NULL
+);
+
 CREATE TABLE Contenu_Message (
 	id_contenu_message		SERIAL PRIMARY KEY,
 	objet_conutenu_message	VARCHAR NOT NULL,
 	contenu_message 		VARCHAR NOT NULL
 );
-CREATE TABLE Type_Place (
-	id_type_place			SERIAL PRIMARY KEY,
-	id_lieu					PRIMARY KEY REFERENCES Lieu,
-	nom_type_place			VARCHAR NOT NULL,
-	capacite_type_place		INTEGER CHECK (capacite_type_place > 0)
-)
-CREATE TABLE Classe_Prix (
-	id_classe_prix 			SERIAL PRIMARY KEY,
-	id_evenement 			PRIMARY KEY REFERENCES Evenement_Culturel,
-	id_type_place 			PRIMARY KEY REFERENCES Type_Place,
-	prix_classe_prix		INTEGER CHECK (prix_classe_prix >= 0)
+CREATE TABLE Message (
+	id_message 				SERIAL PRIMARY KEY,
+	id_membre				INTEGER REFERENCES Membre,
+	date_message			TIMESTAMP NOT NULL,
+	id_contenu_message 		INTEGER REFERENCES Contenu_Message
 );
 
 CREATE TABLE Evenement_Culturel (
 	id_evenement			SERIAL PRIMARY KEY,
-	nom_evenement			VARCHAR NOT NULL UNIQUE,
+	nom_evenement			VARCHAR NOT NULL UNIQUE
 );
 CREATE TABLE Piece_Theatre (
     genre_piece				VARCHAR NOT NULL
@@ -41,46 +52,29 @@ CREATE TABLE Exposition (
 CREATE TABLE Festival (
 ) INHERITS (Evenement_Culturel);
 
-CREATE DATE (
-	id_date					SERIAL PRIMARY KEY,
-	date_debut				DAYTIME NOT NULL,
-	date_fin				DAYTIME NOT NULL,
-	id_evenement			PRIMARY KEY REFERENCES Evenement_Culturel
+CREATE TABLE Type_Place (
+	id_type_place			SERIAL PRIMARY KEY,
+	id_lieu					INTEGER REFERENCES Lieu,
+	nom_type_place			VARCHAR NOT NULL,
+	capacite_type_place		INTEGER CHECK (capacite_type_place > 0)
 );
 
--- Safir
-
-CREATE TABLE Lieu (
-       id_lieu INTEGER PRIMARY KEY,
-       nom_lieu VARCHAR NOT NULL,
-       adress_lieu  VARCHAR NOT NULL,
+CREATE TABLE Classe_Prix (
+	id_classe_prix 			SERIAL PRIMARY KEY,
+	id_evenement 			INTEGER REFERENCES Evenement_Culturel,
+	id_type_place 			INTEGER REFERENCES Type_Place,
+	prix_classe_prix		INTEGER CHECK (prix_classe_prix >= 0)
 );
 
-CREATE TABLE Ville (
-       id_ville INTEGER PRIMARY KEY,
-       code_postal_ville INTEGER NOT NULL,
-       nom_ville VARCHAR NOT NULL,
+CREATE TABLE Date_Evenement (
+	id_date_evenemnt		SERIAL PRIMARY KEY,
+	date_debut_evenement	TIMESTAMP NOT NULL,
+	date_fin_evenement		TIMESTAMP NOT NULL,
+	id_evenement			INTEGER REFERENCES Evenement_Culturel
 );
-
-
-CREATE TABLE Membre (
-       id_membre INTEGER PRIMARY KEY,
-       nom_membre VARCHAR NOT NULL,
-       prenom_membre VARCHAR NOT NULL,
-       password_membre VARCHAR NOT NULL,
-       pseudo_membre VARCHAR NOT NULL,
-       mail_membre VARCHAR NOT NULL,
-       adresse_membre  VARCHAR NOT NULL,
-       code_postal_membre  INTEGER NOT NULL,
-       estGarcon_membre BOOLEAN NOT NULL
-);
-
-
 
 CREATE TABLE Avoir (
-       id_avoir INTEGER PRIMARY KEY,
-       FOREIGN KEY (id_membre) REFERENCES Membre (id_membre),
-       montant INTEGER job_admin NOT NULL
+       id_avoir 			SERIAL PRIMARY KEY,
+       id_membre			INTEGER REFERENCES Membre (id_membre),
+       montant 				INTEGER NOT NULL
 );
-
-
