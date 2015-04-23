fa4dea5f1fce90cb8310938b9a5359d1558225ca
Safir MEDJEBER <medjeber@informatique.univ-paris-diderot.fr>
2015-04-23 11:57:18 +0200

    debut remplissage

--- init_base.sql
+++ init_base.sql
@@ -2,28 +2,28 @@
 \i drop_base.sql
 
 CREATE TABLE Ville (
-	   id_ville				SERIAL PRIMARY KEY,
-	   code_postal_ville	INTEGER NOT NULL UNIQUE,
+	   id_ville			SERIAL PRIMARY KEY,
+	   code_postal_ville		INTEGER NOT NULL UNIQUE,
 	   nom_ville 			VARCHAR NOT NULL UNIQUE
 );
 
 CREATE TABLE Lieu (
-	   id_lieu				SERIAL PRIMARY KEY,
+	   id_lieu			SERIAL PRIMARY KEY,
 	   nom_lieu 			VARCHAR NOT NULL UNIQUE,
 	   adresse_lieu  		VARCHAR NOT NULL,
-	   id_ville				INTEGER REFERENCES Ville
+	   id_ville			INTEGER REFERENCES Ville
 );
 
 CREATE TABLE Membre (
 	   id_membre 			SERIAL PRIMARY KEY,
 	   nom_membre 			VARCHAR NOT NULL,
 	   prenom_membre 		VARCHAR NOT NULL,
-	   sexe_membre			VARCHAR(1) CHECK(sexe_membre = 'F' or sexe_membre = 'G'),
+	   sexe_membre			VARCHAR(1) CHECK(sexe_membre = 'F' or sexe_membre = 'H'),
 	   password_membre 		VARCHAR NOT NULL,
 	   pseudo_membre 		VARCHAR NOT NULL UNIQUE,
 	   mail_membre 			VARCHAR NOT NULL UNIQUE,
 	   adresse_membre  		VARCHAR NOT NULL,
-	   code_postal_ville	INTEGER REFERENCES Ville
+	   code_postal_ville		INTEGER REFERENCES Ville(id_ville)
 );
 
 CREATE TABLE Administrateur (
