57f35247f7c7df512f1b67adddd34c62a74747b4
BlisterBoy <mehdi.abelha@gmail.com>
2015-04-28 10:58:30 +0200

    Modif trigger date_evenement

--- triggers.sql
+++ triggers.sql
@@ -5,31 +5,29 @@ CREATE OR REPLACE FUNCTION trigger_date_evenement()
 RETURNS TRIGGER
 AS $$
 DECLARE
-	compteur BIGINT;
+	compteur_chevauchement BIGINT;
 BEGIN 
-	-- On liste les evenements en conflit avec NEW
-	SELECT count(*) INTO compteur
+	-- Une nouvelle date n'en chevauche pas une autre
+	SELECT count(*) INTO compteur_chevauchement
 	FROM 		 Date_Evenement
 	NATURAL JOIN Evenement_Culturel
 	WHERE 
 		id_evenement = NEW.id_evenement
 		AND
 		(
-			-- TODO : Verifier les capacités de l'evenement
-
 			-- La nouvelle programmation ne doit pas en chevaucher une autre
 			(NEW.date_evenement BETWEEN date_evenement AND date_evenement + duree_evenement)
 			OR 
 			(NEW.date_evenement + duree_evenement BETWEEN date_evenement AND date_evenement + duree_evenement)
 		)
 	;
-
-	-- Une nouvelle date n'en chevauche pas une autre
-	IF compteur > 0 THEN
+	IF compteur_chevauchement > 0 THEN
 		RAISE 'Erreur : Les date de l evenement chevauche une autre programmation';
 		RETURN NULL;
+	END IF;
+
 	-- Une nouvelle date doit être dans le futur
-	ELSIF NEW.date_evenement < TODAY() THEN
+	IF NEW.date_evenement < TODAY() THEN
 		RAISE 'Erreur : date_debut avant aujourd hui';
 		RETURN NULL;
 	END IF;
