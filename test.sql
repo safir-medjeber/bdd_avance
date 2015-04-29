209a580cb2f8cf0b820b22440f57cb620edc31dc
Safir MEDJEBER <medjeber@informatique.univ-paris-diderot.fr>
2015-04-29 13:09:37 +0200

    debut fonction de recherche et stat dans le fichier test/sql

--- test.sql
+++ test.sql
@@ -1,88 +1,55 @@
 
--- drop function nbMembreF();
+-- Determine le ratio homme, femme sur la plateforme
+CREATE OR REPLACE function ratio_homme_femme_sur_plateforme() RETURNS text as $$
+DECLARE
+	femme INTEGER := 0;
+	total INTEGER := 0;
+	pourcentageH  INTEGER :=0;
+	pourcentageF  INTEGER :=0;
+	reponse text := 0;
+BEGIN
+	SELECT count(*)  INTO femme FROM membre WHERE sexe_membre='F';
+    	SELECT count(*)  INTO total FROM membre;	
+    	pourcentageF:= (femme*100)/total;
+    	pourcentageH := 100 - pourcentageF ;
+    	reponse := 'pourcentage d''homme: ' || pourcentageH || chr(10) || 'pourcentage de femme: ' || pourcentageF;
+	RETURN reponse;
+END
+$$ LANGUAGE plpgsql;
 
--- Create or replace  function nbMembreF() RETURNS TABLE(prenom_membre varchar) as $$
--- begin
--- return query select membre.prenom_membre from membre where sexe_membre='F';
--- end
--- $$ language plpgsql;
-
--- --select * from nbMembreF();
-
-
--- drop function partMembreF();
-
--- Create or replace  function partMembreF() RETURNS text as $$
--- declare
--- femme INTEGER := 0;
--- total INTEGER := 0;
--- pourcentage  INTEGER :=0;
--- x text;
--- begin
---     select count(*)  into femme from membre where sexe_membre='F';
---     select count(*)  into total from membre;	
---     pourcentage := (femme*100)/total;
---     x := pourcentage || ' %';
--- return x;
--- end
--- $$ language plpgsql;
-
--- select partMembreF();
+--select ratio_homme_femme_sur_plateforme();
 
 
 
 
-
--- Create or replace  function partMembreH() RETURNS text as $$
--- declare
--- homme INTEGER := 0;
--- total INTEGER := 0;
--- pourcentage  INTEGER :=0;
--- x text;
--- begin
---     select count(*)  into homme from membre where sexe_membre='H';
---     select count(*)  into total from membre;	
---     pourcentage := (homme*100)/total;
---     x := pourcentage || ' %';
--- return x;
--- end
--- $$ language plpgsql;
-
--- select partMembreH();
+--drop function searchEvent(boolean, boolean, boolean);
 
 
-
-drop function searchEvent(boolean, boolean, boolean);
-
-
-Create or replace  function searchEvent(event_theatre boolean, event_exposition boolean,  event_festival boolean, date_debut date, date_fin date)
+CREATE OR REPLACE  FUNCTION searchEvent(event_theatre boolean, event_exposition boolean,  event_festival boolean, date_debut date, date_fin date, prix_min INTEGER, prix_max INTEGER, type_evenement TEXT)
        RETURNS TABLE(nom_evenement varchar) as $$
 DECLARE
        requete text := 'select evenement_culturel.nom_evenement from evenement_culturel';
 BEGIN
 	CASE
 		WHEN event_theatre THEN
-		     requete := requete ||' left join piece_theatre on evenement_culturel.nom_evenement=piece_theatre.nom_evenement';
+		     requete := requete ||' NATURAL JOIN piece_theatre';
 		WHEN event_theatre THEN
-		     requete := requete ||' left join exposition on evenement_culturel.nom_evenement=exposition.nom_evenement';
+		     requete := requete ||' NATURAL JOIN exposition ';
 		WHEN event_festival THEN	     
-		     requete := requete ||' left join festival on evenement_culturel.nom_evenement=festival.nom_evenement';
+		     requete := requete ||' NATURAL JOIN festival ';
 	END CASE;
-	RETURN query execute requete ;
+	requete :=  requete || 'NATURAL JOIN date_evenement WHERE date_evenement BETWEEN '''|| date_debut ||''' AND '''|| date_fin ||''' ';
+	requete :=  requete || 'AND prix_date_evenement BETWEEN '|| prix_min || ' AND ' || prix_max || ' ';
+	requete :=  requete || 'AND type_festival like ''%'|| type_evenement ||'%''';
+
+	RETURN QUERY EXECUTE requete ;
 END
 
 $$ language plpgsql;
 
---select searchEvent(true, false, false, '10-10-2000', '10-10-2001');
+SELECT searchEvent(false, false, true, '2015-01-12', '2015-04-17', 60, 80, 'cinema');
 
 
 
-\echo requetes
--- select evenement_culturel.nom_evenement from evenement_culturel
---  	left join piece_theatre on evenement_culturel.nom_evenement=piece_theatre.nom_evenement  
---  	left join festival on evenement_culturel.nom_evenement=festival.nom_evenement;    
 
 
- select evenement_culturel.nom_evenement from evenement_culturel
- 	inner join date_evenement
-	on date_evenement.id_evenement= evenement_culturel.id_evenement;
