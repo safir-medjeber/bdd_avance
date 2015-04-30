fc23ea9ba83ea16a28da2cba8ce5ce19ade05589
Safir MEDJEBER <medjeber@informatique.univ-paris-diderot.fr>
2015-04-30 14:46:13 +0200

    search_event

--- search_event.sql
+++ search_event.sql
@@ -1,8 +1,8 @@
 CREATE OR REPLACE  FUNCTION searchEvent(event_theatre boolean, event_exposition boolean,  event_festival boolean, date_debut date, date_fin date, prix_min INTEGER, prix_max INTEGER, type_evenement TEXT)
 
-RETURNS TABLE(nom_evenement varchar, date_evenement TIMESTAMP) as $$
+RETURNS TABLE(nom_evenement varchar, date_evenement TIMESTAMP, 	prix_date_evenement INTEGER) as $$
 DECLARE
-       requete text := 'select nom_evenement, date_evenement from evenement_culturel ';
+       requete text := 'select nom_evenement, date_evenement,	prix_date_evenement from evenement_culturel ';
 BEGIN
 
 
@@ -11,24 +11,24 @@ BEGIN
 	   requete := requete ||'natural join(';
 
 	   IF event_theatre THEN
-	   	requete := requete ||'SELECT id_evenement, genre_piece from piece_theatre';
+	   	requete := requete ||'SELECT id_evenement, genre_piece  as genre from piece_theatre';
 	   END IF;
 
 
 	   IF event_exposition  THEN
 	      IF event_theatre  THEN
-	   	requete := requete ||' UNION SELECT id_evenement, type_exposition from exposition';
+	   	requete := requete ||' UNION SELECT id_evenement, type_exposition as genre from exposition';
 	      ELSE
-		requete := requete ||'SELECT id_evenement, type_exposition from exposition';
+		requete := requete ||'SELECT id_evenement, type_exposition  as genre from exposition';
 	      END IF;
 	   END IF;
 
 
 	   IF event_festival THEN	 
 	      IF event_theatre OR event_exposition  THEN
-	 	requete := requete ||' UNION SELECT id_evenement, type_festival from festival';
+	 	requete := requete ||' UNION SELECT id_evenement, type_festival as genre from festival';
 	      ELSE
-		requete := requete ||'SELECT id_evenement, type_festival from festival';
+		requete := requete ||'SELECT id_evenement, type_festival as genre from festival';
 	      END IF;
 	   END IF;
 
@@ -71,24 +71,30 @@ BEGIN
 
 ------------------------gestion genre-------------------------------------------
 	IF type_evenement IS NOT NULL THEN
-	   requete := requete || 'AND ';  
+
+ 	IF date_debut IS NOT NULL OR date_fin IS NOT NULL OR prix_min IS NOT NULL OR prix_max IS NOT NULL THEN
+	        requete := requete || 'AND ';
+	   ELSE
+		requete := requete || 'WHERE ';
+	   END IF;
+	   
 	   IF event_theatre THEN
-	       requete :=  requete || 'genre_piece like ''%'|| type_evenement ||'%''';
+	       requete :=  requete || 'genre like ''%'|| type_evenement ||'%''';
 	   END IF;
 
 	   IF event_exposition  THEN
 	      IF event_theatre  THEN 
-	      	 requete :=  requete || ' AND genre_piece like ''%'|| type_evenement ||'%''';
+	      	 requete :=  requete || ' AND genre like ''%'|| type_evenement ||'%''';
 	      ELSE 
- 	         requete :=  requete || ' type_exposition like ''%'|| type_evenement ||'%''';
+ 	         requete :=  requete || ' genre like ''%'|| type_evenement ||'%''';
 	      END IF;
 	   END IF;
 
 	  IF event_festival THEN	 
 	      IF event_theatre OR event_exposition  THEN
-	        requete :=  requete || ' AND genre_piece like ''%'|| type_evenement ||'%''';
+	        requete :=  requete || ' AND genre like ''%'|| type_evenement ||'%''';
 	      ELSE 
- 	        requete :=  requete || ' type_festival like ''%'|| type_evenement ||'%''';
+ 	        requete :=  requete || ' genre like ''%'|| type_evenement ||'%''';
 	      END IF;
 	  END IF;
 	END IF;
